763e1a7598c0639d3f9a804200728cc8e1699c3d
leanowl <manis.kasyap@gmail.com>
2022-07-26 22:40:48 +0400

    fixing auth route

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import team from './team';
+import auth from './auth';
 
 const router = express.Router();
 
 router.use('/team', team);
+router.use('/auth', auth);
 
 export default router;
